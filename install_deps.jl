0012e2c459a92461f47923c3c7e9357e8154087b
ananmkabaha <86683126+ananmkabaha@users.noreply.github.com>
2025-04-13 12:10:19 +0300

    Create install_deps.jl

---
+++ install_deps.jl
@@ -0,0 +1,12 @@
+using Pkg
+
+Pkg.add("Gurobi")
+Pkg.add("PyCall")
+Pkg.add("PyPlot")
+Pkg.add("Images")
+Pkg.add("JuMP")
+Pkg.add("MathOptInterface")
+Pkg.add("Memento")
+Pkg.add("DocStringExtensions")
+Pkg.add("ProgressMeter")
+Pkg.add("ArgParse")
